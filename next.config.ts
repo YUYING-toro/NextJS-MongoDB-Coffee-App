0f2a9c36911300e3e554f0d440f801660c796b1b
YUYING-toro <candy80523@hotmail.com>
2026-01-07 10:40:35 -0500

    fix: Error in Force-static export const revalidate not configured on route

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* https://nextjs.org/docs/pages/guides/static-exports */
-  output: 'export',
+  // output: 'export',
     images: {
     unoptimized: true, // Close Image Optimization API since export mode does not support it
   },
